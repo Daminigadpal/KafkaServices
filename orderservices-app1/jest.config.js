2c01cfc2e1702a6ea9113788ae3535d5fab6eb36
Daminigadpal <saanvi.gadpal@gmail.com>
2024-06-22 15:14:14 +0530

    changes

---
+++ orderservices-app1/jest.config.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     testMatch: ['**/*.test.js']
+//   };
+   
+
+module.exports = {
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+};
